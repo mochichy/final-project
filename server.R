62ec5546be22ba5731397d191e80b22149bfc7ac
吴限 <wuxian@D-108-179-184-20.dhcp4.washington.edu>
2017-05-31 14:04:11 -0700

    Add map part

--- server.R
+++ server.R
@@ -15,4 +15,13 @@ shinyServer(function(input, output) {
     return(BuildLineChart(data, input$var))
   })
   
+  output$map <- renderPlotly({
+    filtered <- data %>% filter(Name == input$name)
+    count <- filtered %>% group_by(State) %>% summarise(sum = sum(Count))
+    g <- list(scope = 'usa', projection = list(type = 'albers usa'), lakecolor = toRGB('white'))
+    plot_geo(count, locationmode = 'USA-states') %>% add_trace(
+      z = ~sum, text = ~State, locations = ~State, color = ~sum, colors = 'Reds') %>%
+      colorbar(title = "count") %>%
+      layout(title = "Count of name in each state",geo = g) 
+  })
 })
--- ui.R
+++ ui.R
@@ -40,6 +40,15 @@ shinyUI(navbarPage(
                plotlyOutput('line')
              )
            )
-  )
+  ),
+  tabPanel("Map",
+          
+           sidebarPanel(
+             textInput("name", label = h3("Search the name"), value = "Mary"), 
+             helpText("Note: If there is no such name, please try another name."),
+             submitButton("Update")
+           ),
+           mainPanel(plotlyOutput('map'))
+           )
   )
 )
