af5a8905ec4c731571df44176e6e54fbd3a6a3ec
吴限 <wuxian@wuxiandeMacBook-Pro.local>
2017-05-31 19:38:04 -0700

    add comments

--- server.R
+++ server.R
@@ -1,38 +1,51 @@
+# server.R
+# load the packages 
 library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
 
-
+# read in data
 data <- read.csv(file='StateNames.csv', header=TRUE, stringsAsFactors = FALSE)
+# source the script
 source('./BuildLineChart.R')
 
 
 shinyServer(function(input, output) {
   
+  # Render a plot object that returns the plot
   output$babyNamesPlot <- renderPlot({
+    # filter to get the data we want 
     plot.data <- data %>% filter(Name == input$select) %>% group_by(Year) %>% summarise(Count = sum(Count))
     return (ggplot(plot.data, aes(x=Year, y=Count)) + geom_point(color='darkblue'))
   })
   
+  # Render a plotly object that returns the line plot
   output$line <- renderPlotly({ 
+    # using the BuildLineChart script
     return (BuildLineChart(data, input$var))
   })
   
+  # Render a plotly object to get the map
   output$map <- renderPlotly({
+    # filter to get the data will be used for the plot
     filtered <- data %>% filter(Name == input$name)
     count <- filtered %>% group_by(State) %>% summarise(sum = sum(Count))
+    # using plotly to get the Choropleth map
     g <- list(scope = 'usa', projection = list(type = 'albers usa'), lakecolor = toRGB('white'))
     plot_geo(count, locationmode = 'USA-states') %>% add_trace(
       z = ~sum, text = ~State, locations = ~State, color = ~sum, colors = 'Reds') %>%
       colorbar(title = "count") %>%
-      layout(title = "Count of name in each state",geo = g) 
+      layout(title = "Count of babies with the chosen name by state",geo = g) 
   })
   
+  # Render a plotly object to get the bar plot
   output$barplot <- renderPlotly({ 
+    # filter to get the data will be used for the plot
     new.data <- data %>% filter(State == input$barplot1)
     new.data.plot <- distinct(new.data,Name,.keep_all=TRUE) 
     new.data.plot <- new.data.plot %>% arrange(desc(Count)) %>% filter(row_number() <=5L)
+    # using plotly to get the bar plot
     graph <- plot_ly(new.data.plot,x=~Name,y=~Count,type="bar") %>%
       layout(title = "Name Popularity by State",xaxis=list(title="Names"),yaxis=list(title="Popularity/Millions"))
   })
--- ui.R
+++ ui.R
@@ -1,3 +1,5 @@
+# ui.R
+# load the packages
 library(shiny)
 library(dplyr)
 library(ggplot2)
@@ -9,11 +11,15 @@ c <- data %>% select(State) %>% unique
 
 
 shinyUI(navbarPage('Baby Names',
-
+  
+  # Tab 1: Information page
   tabPanel('Information Page',
+           # title
            titlePanel("Introduction"),
+           # Create sidebar layout
            sidebarLayout(
              sidebarPanel(),
+             # show the information in main panel
              mainPanel(
                h6("Episode IV", align = "center"),
                h6("A NEW HOPE", align = "center"),
@@ -25,8 +31,10 @@ shinyUI(navbarPage('Baby Names',
              )
            )
            ),
-                   
+  
+  # Tab 2: Name with different spellings                    
   tabPanel('Name with Different Spellings',
+           # title
            titlePanel('Names'),
            # Create sidebar layout
            sidebarLayout(
@@ -46,34 +54,52 @@ shinyUI(navbarPage('Baby Names',
            )
   ),
   
+  # Tab 3: The trend of Baby names by year
   tabPanel('Baby Names by year',
+           # title
            titlePanel("Trend of Baby Names"),
+           # Create sidebar layout 
            sidebarLayout(
+             # sidebar panel
              sidebarPanel(
+               # Input to select a name 
                selectInput("select", label = h3("Select name"), 
                            choices = choices, 
                            selected = 1)),
+             # display the plot
              mainPanel(
                plotOutput("babyNamesPlot")
              ))
   ),
-
+  
+  # Tab 4: The Choropleth map shows the count of name in each state
   tabPanel("Map",
+           # title
            titlePanel('Map'),
+           # Create sidebar layout
            sidebarLayout(
+             # sidebar panel
              sidebarPanel(
+               # Search bar: type the name in the box to see the map
                textInput("name", label = h3("Search the name"), value = "Mary"), 
-               helpText("Note: If there is no such name, please try another name.")
+               helpText("Note: Note: Type the name into the search box to see how many babies get that name in each state. 
+                        If there is no such name you want to see, please try another one.")
              ),
+             # display the map
              mainPanel(
                plotlyOutput('map')
              ))
   ),
   
+  # Tab 5: The barplot to show the top 5 popular name by state
   tabPanel("State Popularity",
+           # title
           titlePanel("State Popularity Top 5"),
+          # Create the sidebar layout 
             sidebarLayout(
+             # sidebar panel
              sidebarPanel(
+               # Input to select a state
                selectInput('barplot1',
                            label ='Select State to View Name Popularity',
                            choices =c ,selected=1)
